fb9e745a3e9197ecab21338dd7846246c4cb811b
Dhone <dhoneakshay39@gmail.com>
2019-06-02 16:59:37 +0530

    updated one with header footer

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -15,18 +15,35 @@ class _LoginPageState extends State<LoginPage> {
       child: CircleAvatar(
         backgroundColor: Colors.transparent,
         radius: 48.0,
-        child: Image.asset('assets/logo.png'),
+        child: Image.asset('assets/tu.PNG'),
       ),
     );
+
+
+
+    final aivs = Padding(
+      padding: EdgeInsets.all(8.0),
+
+      child: Text(
+
+        'AIVS',
+        style: TextStyle(fontSize: 28.0, color: Colors.black,fontWeight: FontWeight.bold),
+        textAlign: TextAlign.center,
+      ),
+    );
+
 
     final email = TextFormField(
       keyboardType: TextInputType.emailAddress,
       autofocus: false,
-      initialValue: 'alucard@gmail.com',
+      initialValue: 'admin@gmail.com',
       decoration: InputDecoration(
         hintText: 'Email',
+
+
         contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(12.0)),
+
       ),
     );
 
@@ -37,31 +54,41 @@ class _LoginPageState extends State<LoginPage> {
       decoration: InputDecoration(
         hintText: 'Password',
         contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(12.0)),
+
       ),
     );
 
     final loginButton = Padding(
-      padding: EdgeInsets.symmetric(vertical: 16.0),
+
+      padding: EdgeInsets.symmetric(vertical: 16.0,horizontal: 50.0),
+
+
       child: RaisedButton(
+
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(24),
+
         ),
         onPressed: () {
           Navigator.of(context).pushNamed(HomePage.tag);
         },
         padding: EdgeInsets.all(12),
-        color: Colors.lightBlueAccent,
-        child: Text('Log In', style: TextStyle(color: Colors.white)),
+        color: Colors.deepOrange[600],
+
+        child: Text('LogIn',style: TextStyle(color: Colors.white)),
+
       ),
     );
 
-    final forgotLabel = FlatButton(
-      child: Text(
-        'Forgot password?',
-        style: TextStyle(color: Colors.black54),
+
+    final foo = Hero(
+      tag: 'hero',
+      child: CircleAvatar(
+        backgroundColor: Colors.transparent,
+        radius: 48.0,
+        child: Image.asset('assets/fr.PNG'),
       ),
-      onPressed: () {},
     );
 
     return Scaffold(
@@ -69,16 +96,19 @@ class _LoginPageState extends State<LoginPage> {
       body: Center(
         child: ListView(
           shrinkWrap: true,
-          padding: EdgeInsets.only(left: 24.0, right: 24.0),
+          padding: EdgeInsets.only(left: 54.0, right: 54.0),
           children: <Widget>[
             logo,
-            SizedBox(height: 48.0),
+            aivs,
+            SizedBox(height: 20.0),
             email,
             SizedBox(height: 8.0),
             password,
-            SizedBox(height: 24.0),
+            SizedBox(height: 14.0),
             loginButton,
-            forgotLabel
+            foo,
+
+
           ],
         ),
       ),
